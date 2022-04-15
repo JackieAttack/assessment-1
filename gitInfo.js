ca7072a5a12b96cb19059b2c3429d7802a48edbb
Jackie Bearnson <Jackie.bearnson@gmail.com>
2022-04-15 11:30:45 -0600

    gitinfo.js completed

--- gitInfo.js
+++ gitInfo.js
@@ -9,7 +9,7 @@
     It should be a string containing your best definition of what Git is.
 */
 
-//CODE HERE
+let gitDefinition = "Git is software that provides version control, tracking changes, and makes it easier to coordinate work between many programmers on a potentially large program using GitHub."
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -17,7 +17,7 @@
     It should be a string containing your best definition of what GitHub is.
 */
 
-//CODE HERE
+let gitHubDefinition = "GitHub is a website that provides remote hosting of git repositories to allow multiple developers to work on the same program or to share your programs with other people."
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -25,7 +25,7 @@
     It should be a string containing your best definition of what 'git init' does.
 */
 
-//CODE HERE
+let gitInitDefinition = "git init is a git command that initializes a local repository in whatever folder you're currently in."
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -33,7 +33,7 @@
     It should be a string containing your best definition of what 'git clone' does.
 */
 
-//CODE HERE
+let gitCloneDefinition = "git clone is a git command that creates a copy of a certain repository or a branch within that repository by providing the URL, making a remote repo into a local repo in whatever directory you're in."
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -41,7 +41,7 @@
     It should be a string containing your best definition of what 'git status' does.
 */
 
-//CODE HERE
+let gitStatusDefinition = "git status is a git command that gives you information about changes to files in a repo, including if any changes have not been commited or if any files have been added or deleted since last commit."
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -52,7 +52,9 @@
     It should be a string containing the code to add all files.
 */
 
-//CODE HERE
+let gitAddDefinition = "git add is a git command that adds files in your working directory to the git staging area, allowing them to be included in your next commit. It's a necissary step to commit your changes and push them to GitHub."
+
+let gitAddCode = "git add ."
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -63,8 +65,9 @@
     It should be a string containing the code to commit using the message "initial commit".
 */
 
-//CODE HERE
+let gitCommitDefinition = "git commit is a git command that saves a snapshot of a repo, tracking any changes and allowing you to go back to that point in the future if you need to. Usually you include a short message explaining the changes since last commit."
 
+let gitCommitCode = git commit -m "Initial commit"
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -72,4 +75,4 @@
     It should be a string containing your best definition of what 'git push' does.
 */
 
-//CODE HERE
+let gitPushDefinition = "git push is a git command that uploads the commits saved in a local repository to a remote repo on Github, allowing you to share changes."
